78be5d33aa1ef3c956883db7fc56399cfd85becb
Joaquin <joaquin.melone.zarauza@gmail.com>
2022-12-27 11:34:59 -0300

    Update index, script, style
    -Added function to check if passwords match or not

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const password = document.querySelector
+// DOM declarations 
+
+const form = document.querySelector("form");
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirm_password");
+
+
+form.addEventListener("submit", (e) => {
+   if (password.value != confirmPassword.value) {
+        alert("Passwords don't match");
+        e.preventDefault();
+   }
+})
+
